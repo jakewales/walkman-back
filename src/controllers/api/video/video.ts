92ab7169a799e94ac19ee0f683d65d1533d4588f
JingeXC <cbb467cp01@163.com>
2018-12-12 17:00:02 +0800

    改进

--- src/controllers/api/video/video.ts
+++ src/controllers/api/video/video.ts
@@ -3,19 +3,21 @@ import { fs } from 'mz';
 
 export default class VideoController {
 	public static async getVideo(ctx: Context) {
-		const filePath = `${process.cwd()}/media/DJI_20181126_141625.mp4`;
+		const videoId = ctx.params.id;
+		const streamType = ctx.params.type;
+		const device = ctx.params.device;
+		const filePath = `${process.cwd()}/media/demo2.mp4`;
 		const status = fs.statSync(filePath);
 		const fileSize = status.size;
-		const range = ctx.request.headers.range;
-
-		if (range) {
-			console.log('parts');
+		const range = ctx.request.headers.range ? ctx.request.headers.range : 'bytes=0-';
+		if (streamType === 'range') {
 			let parts = range.replace(/bytes=/, '').split('-');
 			let start = parseInt(parts[0], 10);
-			let end = parts[1] ? parseInt(parts[1], 10): start + 999999;
+			const chunkStep = device === 'Android' ? Math.ceil((fileSize - start) / 3 > 1048575 ? (fileSize - start) / 3 : 1048575) : 524287;
+			let end = parts[1] ? parseInt(parts[1], 10): start + chunkStep;
 
 			end = end > fileSize - 1 ? fileSize - 1 : end;
-			
+
 			let chunksize = (end - start) + 1;
 			let file = fs.createReadStream(filePath, {start, end});
 			const head = {
@@ -28,7 +30,6 @@ export default class VideoController {
 			ctx.status = 206;
 			ctx.body = file;
 		} else {
-			console.log('all');
 			let head = {
 				'content-Length': String(fileSize),
 				'Content-Type': 'video/mp4'
